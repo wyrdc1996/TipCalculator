20faee5c2bec0e40fd2af64194ebae8891124d6c
Haitao Huang <hht1996@hotmail.com>
2019-12-22 04:24:19 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Haitao Huang on 12/22/19.
+//  Copyright © 2019 Haitao Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
